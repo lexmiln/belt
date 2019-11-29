674ad768a387e1a6ab89e9663be453411acdad3e
Alex Macmillan <alex@x13n.com>
2019-11-29 11:44:04 -0600

    Prettier.js

--- main.js
+++ main.js
@@ -1,64 +1,61 @@
-
-var blessed = require('blessed');
-var contrib = require('blessed-contrib');
+var blessed = require("blessed");
+var contrib = require("blessed-contrib");
 
 var screen = blessed.screen({
   smartCSR: true
 });
 
-screen.title = 'Hi Title';
+screen.title = "Hi Title";
 
 var log = contrib.log({
   fg: "green",
   selectedFg: "green",
-  label: 'Server Log'
+  label: "Server Log"
 });
 
-log.log("new log line")
+log.log("new log line");
 
 var box = blessed.box({
-  top: 'center',
-  left: 'right',
-  width: '30%',
-  height: '50%',
+  top: "center",
+  left: "right",
+  width: "30%",
+  height: "50%",
   tags: true,
   border: {
-    type: 'line'
+    type: "line"
   },
   style: {
-    fg: 'white',
-    bg: 'magenta',
+    fg: "white",
+    bg: "magenta",
     border: {
-      fg: '#f0f0f0'
+      fg: "#f0f0f0"
     },
     hover: {
-      bg: 'green'
-    },
-  },
+      bg: "green"
+    }
+  }
 });
 
 box.append(log);
 
 var donut = contrib.donut({
-  label: 'Test',
+  label: "Test",
   radius: 16,
   arcWidth: 4,
-  remainColor: 'black',
+  remainColor: "black",
   yPadding: 2,
-  data: [
-    {percent: 80, label: 'web1', color: 'green'}
-  ]
+  data: [{ percent: 80, label: "web1", color: "green" }]
 });
 
 screen.append(donut);
 screen.append(box);
 
-screen.key(['escape', 'q', 'C-c'], function() {
+screen.key(["escape", "q", "C-c"], function() {
   return process.exit(0);
 });
 
-box.key('enter', function() {
-  box.setContent('{center}Centered?!{/center}');
+box.key("enter", function() {
+  box.setContent("{center}Centered?!{/center}");
 
   screen.render();
 });
