c00ea082465326ab51e30c409a66e115375d34aa
Alex Macmillan <alex@x13n.com>
2019-11-29 11:38:54 -0600

    More environment setup

--- main.js
+++ main.js
@@ -1,57 +1,66 @@
+
 var blessed = require('blessed');
 var contrib = require('blessed-contrib');
 
 var screen = blessed.screen({
-    smartCSR: true
+  smartCSR: true
 });
 
 screen.title = 'Hi Title';
 
-var box = blessed.box({
-    top: 'center',
-    left: 'center',
-    width: '50%',
-    height: '50%',
-    content: 'Hello {bold}world{/bold}, now we\'re cooking with {black-fg}magenta{/black-fg}!',
-    tags: true,
-    border: {
-	type: 'line'
-    },
-    style: {
-	fg: 'white',
-	bg: 'magenta',
-	border: {
-	    fg: '#f0f0f0'
-	},
-	hover: {
-	    bg: 'green'
-	},
-    },
+var log = contrib.log({
+  fg: "green",
+  selectedFg: "green",
+  label: 'Server Log'
 });
 
-screen.append(box);
+log.log("new log line")
+
+var box = blessed.box({
+  top: 'center',
+  left: 'right',
+  width: '30%',
+  height: '50%',
+  tags: true,
+  border: {
+    type: 'line'
+  },
+  style: {
+    fg: 'white',
+    bg: 'magenta',
+    border: {
+      fg: '#f0f0f0'
+    },
+    hover: {
+      bg: 'green'
+    },
+  },
+});
+
+box.append(log);
 
 var donut = contrib.donut({
-    label: 'Test',
-    radius: 16,
-    arcWidth: 1,
-    remainColor: 'black',
-    yPadding: 2,
-    data: [
-	{percent: 80, label: 'web1', color: 'green'}
-    ]
+  label: 'Test',
+  radius: 16,
+  arcWidth: 4,
+  remainColor: 'black',
+  yPadding: 2,
+  data: [
+    {percent: 80, label: 'web1', color: 'green'}
+  ]
 });
 
 screen.append(donut);
+screen.append(box);
 
-screen.key(['escape', 'q', 'C-c'], function(ch, key) {
-    return process.exit(0);
+screen.key(['escape', 'q', 'C-c'], function() {
+  return process.exit(0);
 });
 
-box.key('enter', function(ch, key) {
-    box.setContent('{center}Centered?!{/center}');
+box.key('enter', function() {
+  box.setContent('{center}Centered?!{/center}');
 
-    screen.render();
+  screen.render();
 });
 
 box.focus();
